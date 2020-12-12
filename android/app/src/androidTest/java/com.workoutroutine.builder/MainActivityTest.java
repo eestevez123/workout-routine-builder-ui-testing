cf2a15102528a043b9225167107e94143a23a9d2
Eddie Estevez <eestevez@wisc.edu>
2020-12-11 18:33:06 -0600

    You got a test to run! Congrats!

--- android/app/src/androidTest/java/com.workoutroutine.builder/MainActivityTest.java
+++ android/app/src/androidTest/java/com.workoutroutine.builder/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.workoutroutine.builder;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.SmallTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -11,11 +12,19 @@ import org.junit.runner.RunWith;
 import kotlin.jvm.JvmField;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+
+import java.util.concurrent.TimeUnit;
+
+
 import static org.hamcrest.Matchers.allOf;
+
 
 
 @RunWith(AndroidJUnit4.class)
@@ -27,8 +36,10 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
-        onView(allOf(withContentDescription("elementId"), isDisplayed())
-        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    public void loginButtonThere() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(10); // wait for 10 seconds to get slash screen away
+        ViewInteraction loginButtonTest = onView(
+                allOf(withContentDescription("LoginButton"), isDisplayed()));
+        loginButtonTest.perform(click());
     }
 }
